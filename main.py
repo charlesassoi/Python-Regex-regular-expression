e486a6c244fe90f55bee8c7fb259d8dd686f559b
charlesassoi <charlesassoi07@gmail.com>
2025-08-15 19:11:48 +0000

    new

---
+++ main.py
@@ -0,0 +1,18 @@
+import re
+s = 'NASA A computer science portal for geeks'
+
+match=re.search(r'portal',s)
+print('start index',match.start())
+print('start index',match.end())
+
+string = """Hello my Number is 123456789 and my friend's number is 987654321"""
+
+regex='\d+'
+match=re.findall(regex,string)
+print(match)
+
+p=re.compile('[a-e]')
+print(p.findall(s))
+print(re.findall('\W',s))
+print(re.sub('Hello','Hi',string,count=1))
+print(re.sub(r'\sand\s',' & ',string))
